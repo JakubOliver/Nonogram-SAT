d0bcf39ea03ab21a1b6a845d994dc2e7a33b6756
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2025-10-24 23:22:31 +0200

    Working CNF encoding
    Encoding works by encoding rows and columns into dnf (finding models)
    and afterwards generating anti models and from there we can
    create cnf.
    In this forme SAT solver can solve in reasonable time nonogram
    with dimension ~10x~10.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 ## Poznámky
 VELKE TODO: najit lepsi způsob na encoding než najít DNF a prehodit 
 
+moznost: najit vsechny modely pro kazdy sloupec a radek a potom jednotlive udělat antimodely a z toho dostaneme CNF pro řádky a sloupce a z toho dokážeme po andování udělat CNF pro celou matici
+
 jazyk: vsechna policka
 
 1. sloupec:
@@ -24,6 +26,24 @@ big or 0...n-(2 + 1 + 2)
             p_j and p_{j+1}
 ```
 
+## Tiny nonogram
+
+![Tiny nonogram](./src/tiny_nonogram.PNG)
+
+### Encoding
+
+```
+5
+2
+4
+3 1
+4 2
+1 1 1
+5
+3 
+1 1
+3
+```
 
 ## Small nonogram
 
@@ -52,4 +72,44 @@ big or 0...n-(2 + 1 + 2)
 1 8
 1 8
 8
+```
+
+time: ~1.6 seconds
+
+## Big nonogram
+
+![Big nonogram](./src/big_nonogram.PNG)
+
+```
+15
+1 1
+8
+3 1
+4 2 1
+7 2 1
+1 4 2 1
+4 1
+1 2 4
+2 1 4
+3 2 1
+4 1 2 1
+1 1 2 2 1
+1 1 1 1
+8
+1 1 1
+1
+2
+1
+2 2
+1 2
+9
+5 1 1 1
+7 1 1 1
+15
+1 1
+1 3 2 1
+1 3 2 2 1
+1 2 1
+1 2 1
+15
 ```
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 
 def load_problem(filename: str) -> list:
@@ -8,12 +10,114 @@ def load_problem(filename: str) -> list:
 
         for i in range(2*n):
             problem[i] = list(map(int, file.readline().split()))
+            problem[i].reverse()
 
     return problem
 
+def model_into_number(model: list) -> int:
+    val = 0
+
+    for i in range(len(model)):
+        val <<= 1
+
+        if model[i]:
+            val += 1
+
+    return val
+
+def number_into_model(val: int, n: int) -> list:
+    model = [False]*n
+
+    i = 0
+    while val > 0:
+        if val & 1:
+            model[i] = True
+
+        val >>= 1
+        i += 1
+
+    return model
+
+def dimacs_cnf(model: list, n: int, i: int, row: bool) -> str:
+    dimacs = []
+
+    for j in range(n):
+        if row:
+            k = i + j + 1
+        else:
+            k = i + n*j + 1
+
+        if model[j]:
+            dimacs.append(-k)
+        else:
+            dimacs.append(k)
+
+    return " ".join(map(str, dimacs))
+
+def generate_cnf(dnf: list, n: int, start: int, row: bool) -> list:
+    cnf = []
+    m = len(dnf)
+
+    model_number = [0] * m
+
+    for i in range(m - 1, -1, -1):
+        model_number[i] = model_into_number(dnf[i])
+
+    print(model_number)
+
+    idx = 0
+
+    with open("./output/cnf.txt", "a", encoding="utf-8") as file:
+        for i in range(1<<n):
+            if i in model_number:
+                print(i)
+                continue
+
+            #cnf.append(number_into_model(i, n))
+            file.write(dimacs_cnf(number_into_model(i, n), n, start, row) + " 0 \n")
+
+    return cnf
+
+def encode_to_dnf_recur(params: list, idx: int, start: int, n: int, all_models: list, active: list) -> None:
+    if idx == len(params):
+        all_models.append(list(active))
+        return
+
+    if idx == len(params) - 1:
+        offset = -1
+    else:
+        offset = 0
+
+    for i in range(start, n - params[idx] - offset):
+        for j in range(i, i + params[idx]):
+            active[j] = True
+
+        encode_to_dnf_recur(params, idx + 1, i + params[idx] + 1, n, all_models, active)
+
+        for j in range(i, i + params[idx]):
+            active[j] = False
+
 def encode(problem: list) -> list:
     cnf = []
+    n = len(problem) // 2
 
+    all_models = []
+    complement = []
+
+    with open("./output/cnf.txt", "w", encoding="utf-8") as file:
+        #file.write(f"p cnf {n**2} \n")
+        pass
+
+    for i in range(n * 2):
+        all_models.clear()
+        encode_to_dnf_recur(problem[i], 0, 0, n, all_models, [False] * n)
+
+        if i < n:
+            cnf.append(generate_cnf(all_models, n, i, False))
+        else:
+            cnf.append(generate_cnf(all_models, n, (i-n)*n, True))
+
+        print(all_models)
 
     return cnf
 
@@ -21,6 +125,7 @@ def main():
     problem = load_problem(sys.argv[1])
 
     print(problem)
+    cnf = encode(problem)
 
 if __name__ == "__main__":
     main()
